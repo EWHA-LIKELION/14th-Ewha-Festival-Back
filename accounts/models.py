764d163e72bd6bb9192e683e8eb7af0e899c45ab
gyk <ganada1375@gmail.com>
2026-01-29 22:49:26 +0900

    WIP

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from string import ascii_lowercase, digits
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    first_name = None
+    last_name = None
+    email = models.EmailField(
+        unique = True,
+        error_messages = {'unique': '이미 존재하는 이메일입니다.'}
+    )
--- configs/settings/base.py
+++ configs/settings/base.py
@@ -60,7 +60,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'configs.wsgi.application'
 
-AUTH_USER_MODEL = 'auth.User'
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
