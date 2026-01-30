280df3fc94e8a6d449cc8f6c5d5e0453c2baa201
0euun <jeyejong70@naver.com>
2026-01-30 20:55:15 +0900

    🔨 booths 앱 추가 (#5)

--- configs/urls.py
+++ configs/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('searchs/', include('searchs.urls')),
+    path('booths/', include('booths.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
