2a6f9fdef811feac2fa24587fd6e5ffa904eca21
0euun <jeyejong70@naver.com>
2026-01-30 22:06:14 +0900

    🔨 Show 모델 추가 (#5)

--- shows/models.py
+++ shows/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from booths.models import BaseProgram
+from utils.choices import ShowCategoryChoices
 
 # Create your models here.
+
+class Show(BaseProgram):
+    category = models.CharField(
+        help_text="카테고리",
+        max_length=10,
+        choices=ShowCategoryChoices.choices,
+    )
