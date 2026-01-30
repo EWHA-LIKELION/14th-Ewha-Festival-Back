812964e0647cc16424d0cd269afeae737f414c4d
0euun <jeyejong70@naver.com>
2026-01-30 23:02:21 +0900

    🔨 ShowScrap 모델 추가 (#5)

--- shows/models.py
+++ shows/models.py
@@ -97,3 +97,24 @@ class ShowReview(models.Model):
 
     def __str__(self):
         return f"{self.user.show.name} - 익명 {self.user.number}"
+
+class ShowScrap(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        help_text="사용자",
+        on_delete=models.CASCADE,
+        related_name="show_scrap",
+    )
+    show = models.ForeignKey(
+        'Show',
+        help_text="공연",
+        on_delete=models.CASCADE,
+        related_name="show_scrap",
+    )
+    created_at = models.DateTimeField(
+        help_text="생성일시",
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f"{self.user.email} - {self.show.name}"
