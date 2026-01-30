82b151789a948979437ee3e94e36f6bdaa15ec9f
0euun <jeyejong70@naver.com>
2026-01-30 23:00:10 +0900

    🔨 ShowReview 모델 추가 (#5)

--- shows/models.py
+++ shows/models.py
@@ -75,3 +75,25 @@ class ShowReviewUser(models.Model):
 
     def __str__(self):
         return f"{self.show.name} - {self.number}"
+
+class ShowReview(models.Model):
+    user = models.ForeignKey(
+        'ShowReviewUser',
+        help_text="공연 후기 작성자",
+        on_delete=models.CASCADE,
+        related_name="show_review",
+    )
+    content = models.TextField(
+        help_text="내용",
+    )
+    created_at = models.DateTimeField(
+        help_text="생성일시",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        help_text="수정일시",
+        auto_now=True,
+    )
+
+    def __str__(self):
+        return f"{self.user.show.name} - 익명 {self.user.number}"
