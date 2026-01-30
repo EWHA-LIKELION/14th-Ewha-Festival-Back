6b85318369be0563fb770800ae19addbf2e54313
0euun <jeyejong70@naver.com>
2026-01-30 20:57:20 +0900

    🔨 shows 앱 추가 (#5)

--- configs/urls.py
+++ configs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('searchs/', include('searchs.urls')),
     path('booths/', include('booths.urls')),
+    path('shows/', include('shows.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
