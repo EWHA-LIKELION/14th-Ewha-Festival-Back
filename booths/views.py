bcf290481c925c4b8019f6b841c13bcd15e141e4
0euun <jeyejong70@naver.com>
2026-02-02 22:59:53 +0900

    🔨 booths 앱 BoothDetailView 뷰 추가 (#8)

--- booths/views.py
+++ booths/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Booth
+from .serializers import BoothSerializer
 
 # Create your views here.
+
+class BoothDetailView(APIView):
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+    
+    def get_object(self, pk):
+        try:
+            return Booth.objects.get(pk=pk)
+        except Booth.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def get(self, request:HttpRequest, pk, format=None):
+        booth = self.get_object(pk)
+        serializer = BoothSerializer(booth)
+        return Response(serializer.data, status=status.HTTP_200_OK)
