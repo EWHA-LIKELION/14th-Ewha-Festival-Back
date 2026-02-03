47808a81b5ff80854e8c7e7352db2b008462207e
0euun <jeyejong70@naver.com>
2026-02-03 19:48:57 +0900

    🔨 작성 시간 관련 time_ago 함수 추가 (#8)

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,5 @@
 from django.utils.deconstruct import deconstructible
+from django.utils import timezone
 
 @deconstructible
 class FilePathBuilder:
@@ -11,3 +12,16 @@ class FilePathBuilder:
     def __call__(self, instance, file_name):
         class_name = instance.__class__.__name__.lower()
         return f"{class_name}/{self.sub_path}/{file_name}"
+
+def time_ago(dt):
+    now = timezone.now()
+    diff = now - dt
+    seconds = int(diff.total_seconds())
+
+    if seconds < 60:
+        return "방금 전"
+    if seconds < 3600:
+        return f"{seconds // 60}분 전"
+    if seconds < 86400:
+        return f"{seconds // 3600}시간 전"
+    return f"{seconds // 86400}일 전"
