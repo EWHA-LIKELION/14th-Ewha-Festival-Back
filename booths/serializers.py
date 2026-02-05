52d6998f2e6e0c365962ee082e2b06f580c7a235
Youngeun Seol <jeyejong70@naver.com>
2026-02-05 19:06:08 +0900

    ✨ 부스 상세 조회 API (#9)
    * 🔨 booths 앱 BoothDetailView 뷰 추가 (#8)
    
    * 🔨 booths 앱 detail 유알엘 추가 (#8)
    
    * 🔨 BoothDetailSerializer 추가 (#8)
    
    * ♻️ BoothDetailSerializer 역참조 이름으로 수정 (#8)
    
    * ♻️ BoothSerializer 2차 수정 (#8)
    
    * 🔨 BoothDetailView 뷰 get 구현 (#8)
    
    * ♻️ BoothDetailSerializer 3차 수정 (#8)
    
    * ♻️ BoothDetailSerializer 4차 수정 (#8)
    
    * 🐛 Location 관련 수정 (#8)
    
    * 🔨 작성 시간 관련 time_ago 함수 추가 (#8)
    
    * ♻️ Notice, Review 작성 시간 코드 수정 (#8)
    
    * ♻️ 시리얼라이저 상속 처리 (#8)

--- booths/serializers.py
+++ booths/serializers.py
@@ -1,1 +1,35 @@
 from rest_framework import serializers
+from .models import Booth, Product, BoothReview, BoothNotice
+from utils.abstract_serializers import BaseProgramDetailSerializer, BaseNoticeSerializer, BaseReviewSerializer
+
+class BoothProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id', 'name', 'description', 'price', 'image', 'is_selling',
+        )
+
+class BoothNoticeSerializer(BaseNoticeSerializer):
+    class Meta(BaseNoticeSerializer.Meta):
+        model = BoothNotice
+
+class BoothReviewSerializer(BaseReviewSerializer):
+    class Meta(BaseReviewSerializer.Meta):
+        model = BoothReview
+
+class BoothDetailSerializer(BaseProgramDetailSerializer):
+    product = serializers.SerializerMethodField()
+
+    class Meta(BaseProgramDetailSerializer.Meta):
+        model = Booth
+        fields = BaseProgramDetailSerializer.Meta.fields + ('product',)
+
+    def get_product(self, obj):
+        products = obj.product.filter(is_selling=True)
+        return BoothProductSerializer(products, many=True).data
+
+    def get_notice_serializer(self): return BoothNoticeSerializer
+    def get_review_serializer(self): return BoothReviewSerializer
+    def get_review_model(self): 
+        from .models import BoothReview
+        return BoothReview
--- booths/views.py
+++ booths/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpRequest, Http404
+from django.db.models import Count
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Booth
+from .serializers import BoothDetailSerializer
 
 # Create your views here.
+
+class BoothDetailView(APIView):
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+    
+    def get_object(self, pk):
+        try:
+            return (
+                Booth.objects
+                .annotate(scraps_count=Count("booth_scrap"))
+                .get(pk=pk)
+            )
+        except Booth.DoesNotExist:
+            raise Http404
+    
+    def get(self, request:HttpRequest, pk, format=None):
+        booth = self.get_object(pk)
+        serializer = BoothDetailSerializer(
+            booth,
+            context={"request": request},
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
