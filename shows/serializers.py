2134cf590d6a14f3e6a313ebf89a8547bd16c5e2
0euun <jeyejong70@naver.com>
2026-02-06 23:47:25 +0900

    🔨 ShowDetailSerializer 추가 (#11)

--- shows/serializers.py
+++ shows/serializers.py
@@ -1,1 +1,35 @@
+from re import S
 from rest_framework import serializers
+from .models import Show, Setlist, ShowReview, ShowNotice
+from utils.abstract_serializers import BaseProgramDetailSerializer, BaseNoticeSerializer, BaseReviewSerializer
+
+class ShowSetlistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Setlist
+        fields = (
+            'id', 'name',
+        )
+
+class ShowNoticeSerializer(BaseNoticeSerializer):
+    class Meta(BaseNoticeSerializer.Meta):
+        model = ShowNotice
+
+class ShowReviewSerializer(BaseReviewSerializer):
+    class Meta(BaseReviewSerializer.Meta):
+        model = ShowReview
+
+class ShowDetailSerializer(BaseProgramDetailSerializer):
+    setlist = ShowSetlistSerializer(many=True)
+
+    class Meta(BaseProgramDetailSerializer.Meta):
+        model = Show
+        fields = tuple(
+            f for f in BaseProgramDetailSerializer.Meta.fields
+            if f != 'host'
+        ) + ('setlist',)
+
+    def get_notice_serializer(self): return ShowNoticeSerializer
+    def get_review_serializer(self): return ShowReviewSerializer
+    def get_review_model(self): 
+        from .models import ShowReview
+        return ShowReview
