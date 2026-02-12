2bfcafaebfb0f6cba0fe50cbfa3ffa7a0d243956
unknown <ganada1375@gmail.com>
2026-02-12 21:09:51 +0900

    ⚰️ 불필요 코드 삭제

--- booths/admin.py
+++ booths/admin.py
@@ -5,29 +5,7 @@ from django.contrib.postgres.forms.ranges import DateTimeRangeField as FormDateT
 
 from .models import Booth, BoothNotice, Product, BoothReviewUser, BoothReview, BoothScrap
 
-
-class BoothAdminForm(forms.ModelForm):
-    # ✅ ArrayField delimiter를 콤마(,)가 아니라 세미콜론(;)로 바꿔서
-    # range 내부의 콤마와 충돌을 피함
-    schedule = SimpleArrayField(
-        base_field=FormDateTimeRangeField(),
-        delimiter=';',
-        required=False,
-    )
-
-    class Meta:
-        model = Booth
-        fields = "__all__"
-
-
-class BoothAdmin(admin.ModelAdmin):
-    form = BoothAdminForm
-
-
-# ✅ 기존 register(Booth) 대신 커스텀 admin으로 등록
-admin.site.register(Booth, BoothAdmin)
-
-# 나머지는 그대로
+admin.site.register(Booth)
 admin.site.register(BoothNotice)
 admin.site.register(Product)
 admin.site.register(BoothReviewUser)
