9933ff017e096a3764e6a4ba4323aedaf7dcd904
unknown <ganada1375@gmail.com>
2026-02-12 14:58:09 +0900

    ✨ BoothpatchSerializer 추가

--- booths/serializers.py
+++ booths/serializers.py
@@ -33,3 +33,46 @@ class BoothDetailSerializer(BaseProgramDetailSerializer):
     def get_review_model(self): 
         from .models import BoothReview
         return BoothReview
+
+class BoothPatchSerializer(serializrs.ModelSerialzier):
+    product = BoothProductSerializer(many = True, required = False)
+    
+    class Meta:
+        model = Booth
+        fields = (
+            'thumbnail', 'name', 'category', 'is_ongoing',
+            'description', 'location_description', 'roadview', 'sns',
+            'host', 'product',
+        )
+        
+    def update(self, instance, validated_data):
+        products_data = validated_data.pip('product', None)
+        
+        with transaction.atomic():
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save()
+            
+            if products_data is not None:
+                for p in products_data:
+                    product_id = p.get('id', None)
+                    
+                    if product_id is None:
+                        Product.objects.create(booth=instance, **p)
+                        continue
+                
+                # 이 booth의 product인지 확인
+                qs = instance.product.filter(id=product_id)
+                if not qs.exists():
+                    raise serializers.ValidationError({
+                        "product": [f"Invalid product id = {product_id} for this booth."]
+                    })
+                    
+                obj = qs.first()
+                for k, v in p.items():
+                    if k == 'id':
+                        continue
+                    setattr(obj, k, v)
+                obj.save()
+                
+        return instance
