704408142f7a7c7546200e242418e2610335c46c
0euun <jeyejong70@naver.com>
2026-02-13 18:04:37 +0900

    🔨 ShowNoticeView 뷰 get 구현 (#18)

--- shows/views.py
+++ shows/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpRequest, Http404
+from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Show
-from .serializers import ShowDetailSerializer
+from .models import Show, ShowNotice
+from .serializers import ShowDetailSerializer, ShowNoticeSerializer
 
 # Create your views here.
 
@@ -29,6 +30,25 @@ class ShowDetailView(APIView):
         show = self.get_object(pk)
         serializer = ShowDetailSerializer(
             show,
+            context={"request": request},
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ShowNoticeView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request: HttpRequest, pk, format=None):
+        get_object_or_404(Show, pk=pk)
+    
+        notices = (
+            ShowNotice.objects
+            .filter(show_id=pk)
+            .order_by('-created_at')
+        )
+
+        serializer = ShowNoticeSerializer(
+            notices,
+            many=True,
             context={"request": request},
         )
         return Response(serializer.data, status=status.HTTP_200_OK)
