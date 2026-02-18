a80b197897e90943f8774f8a5ddc0c534c18f68f
0euun <jeyejong70@naver.com>
2026-02-18 22:47:12 +0900

    🔨 MyDataView 추가 (#20)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.db.models import Count
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import MyDataSerializer
 
 # Create your views here.
+
+class MyDataView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request:HttpRequest, format=None):
+        user = request.user
+
+        booths = user.permission_booth.annotate(
+            scrap_count=Count("booth_scrap"),
+            review_count=Count("booth_review_user"),
+        ).order_by("name")
+
+        shows = user.permission_show.annotate(
+            scrap_count=Count("show_scrap"),
+            review_count=Count("show_review_user"),
+        ).order_by("name")
+
+        serializer = MyDataSerializer(
+            user,
+            context={"request": request, "managed_booths": booths, "managed_shows": shows},
+        )
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
