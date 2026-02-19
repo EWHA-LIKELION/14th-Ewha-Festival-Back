4a50709174ecd24f770ffa8e55c6c562640f1dfc
zziminally <zziminally@gmail.com>
2026-02-20 03:57:01 +0900

    🔨 검색 시리얼라이저 추가 (#21)

--- searchs/serializers.py
+++ searchs/serializers.py
@@ -1,1 +1,11 @@
-from rest_framework import serializers
+from utils.abstract_serializers import BaseProgramListSerializer
+from booths.models import Booth
+from shows.models import Show
+
+class BoothSearchSerializer(BaseProgramListSerializer):
+    class Meta(BaseProgramListSerializer.Meta):
+        model = Booth
+    
+class ShowSearchSerializer(BaseProgramListSerializer):
+    class Meta(BaseProgramListSerializer.Meta):
+        model = Show
