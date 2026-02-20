a6a90928356aa31b6d9a69ba503b710e6cc70502
zziminally <zziminally@gmail.com>
2026-02-20 14:49:45 +0900

    🔨 스크랩북 - 검색 엔드포인트 추가 (#22)

--- searchs/urls.py
+++ searchs/urls.py
@@ -4,4 +4,9 @@ from .views import *
 app_name = 'searchs'
 
 urlpatterns = [
+<<<<<<< Updated upstream
+=======
+    path('', SearchView.as_view()),
+    path('scrapbook/', ScrapbookSearchView.as_view()),
+>>>>>>> Stashed changes
 ]
