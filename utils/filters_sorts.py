b00480877398cc6ce716f64b272d6d084eea6248
zziminally <zziminally@gmail.com>
2026-03-05 10:46:59 +0900

    🔨 정렬 로직 추가 (#32)

--- utils/filters_sorts.py
+++ utils/filters_sorts.py
@@ -69,3 +69,55 @@ def base_filter(qs, params, *, program: str):
         ).filter(has_overlap_date=True)
     
     return qs
+
+# 정렬
+BOOTH_BUILDING_PRIORITY = [
+    LocationChoices.HUMAN_ECOLOGY_BUILDING,
+    LocationChoices.STUDENT_UNION,
+    LocationChoices.HAK_GWAN,
+    LocationChoices.EWHA_POSCO_BUILDING,
+    LocationChoices.MAIN_GATE,
+    LocationChoices.WELCH_RYANG_AUDITORIUM,
+    LocationChoices.HYUUT_GIL,
+    LocationChoices.EDUCATION_BUILDING,
+    LocationChoices.EWHA_SHINSEGAE_BUILDING,
+    LocationChoices.SPORT_TRACK,
+]
+
+def base_sort(qs, sorting: str | None, *, program: str):
+    sorting = (sorting or "").lower().strip()
+    
+    # 스크랩순
+    if sorting == "scrap":
+        return qs.order_by("-scraps_count", "id")
+    
+    # 이름순
+    if sorting == "name":
+        return qs.order_by("name", "id")
+    
+    # 부스 default - 번호순
+    if program == "booth" and sorting in ("number", ""):
+        whens = [
+            When(location__building=building, then=Value(i))
+            for i, building in enumerate(BOOTH_BUILDING_PRIORITY)
+        ]
+
+        sql_unnest = "(SELECT MIN(lower(r)) FROM unnest(schedule) AS r)"
+
+        return qs.annotate(
+            building_priority_index=Case(
+                *whens,
+                default=Value(999),
+                output_field=IntegerField(),
+            ),
+            unnest_time=RawSQL(sql_unnest, []),
+        ).order_by("building_priority_index", "location__number", "unnest_time", "id")
+
+    # 공연 default - 시간순
+    if program == "show" and sorting in ("time", ""):
+        sql_unnest = "(SELECT MIN(lower(r)) FROM unnest(schedule) AS r)"
+        return qs.annotate(
+            unnest_time=RawSQL(sql_unnest, [])
+        ).order_by("unnest_time", "id")
+        
+    return qs
