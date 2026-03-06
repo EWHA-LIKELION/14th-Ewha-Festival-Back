cf171b3a50c2569a2addaf29dea18e10cb752ef1
zziminally <zziminally@gmail.com>
2026-03-06 19:04:32 +0900

    🔨 BoothSearchSerializer 추가 (#21)

--- searchs/serializers.py
+++ searchs/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from utils.abstract_serializers import BaseProgramListSerializer
+from booths.models import Booth
+
+class BoothSearchSerializer(BaseProgramListSerializer):
+    class Meta(BaseProgramListSerializer.Meta):
+        model = Booth
