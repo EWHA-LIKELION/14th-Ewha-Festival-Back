5141b62ada34c4994ab488bc6838b68317fc4ace
gyuuuuri <gyuri113@ewha.ac.kr>
2026-03-29 00:52:00 +0900

    ✏️ 앱 이름 오류 수정

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -145,7 +145,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'users.authentication.CookieJWTAuthentication',
+        'accounts.authentication.CookieJWTAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
