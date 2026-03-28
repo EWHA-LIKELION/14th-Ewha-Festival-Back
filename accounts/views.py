f9f63989ac36712efe3c9b7169cc145055d26439
gyuuuuri <168539800+gyuuuuri@users.noreply.github.com>
2026-03-28 22:33:37 +0900

    카카오 로그인 테스트코드 수정 (#67)

--- accounts/views.py
+++ accounts/views.py
@@ -136,15 +136,15 @@ class KakaoCallbackView(APIView):
                 "access",
                 str(refresh.access_token),
                 httponly=True,
-                samesite="Lax",
-                secure=False,
+                samesite="None",
+                secure=True,
             )
             response.set_cookie(
                 "refresh",
                 str(refresh),
                 httponly=True,
-                samesite="Lax",
-                secure=False,
+                samesite="None",
+                secure=True,
             )
             return response
 
