7a90a6f76785173dc56c409247ebafaca14f6b0b
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-04-12 21:07:33 +0900

    🗃️ ShowManager 추가 (#86)
    - DB 레벨에서 is_ongoing을 계산하여 반환한다. 모든 조회에서 is_ongoing을 포함한다.

--- shows/models.py
+++ shows/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.contrib.postgres.fields import DateTimeRangeField
 from utils.abstract_models import BaseProgram, BaseNotice, BaseReviewUser, BaseReview, BaseScrap
 from utils.choices import ShowCategoryChoices
+from .managers import ShowManager
 
 class Show(BaseProgram):
+    schedule = DateTimeRangeField(),
     category = models.CharField(
         help_text="카테고리",
         max_length=10,
         choices=ShowCategoryChoices.choices,
         null=True,
     )
+
+    objects = ShowManager()
 
 class ShowNotice(BaseNotice):
     show = models.ForeignKey(
