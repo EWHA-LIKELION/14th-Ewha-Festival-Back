21db19fb05739090bc42a775c86bd7975df2299f
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-04-13 00:17:03 +0900

    🐛 RangeField는 fully를 사용해야 함 (#86)
    - DateTimeRangeField에서 `__lower__gt`, `__upper__lt` 같은 bound 직접 접근 방식은 When 조건 내 Q 객체로 변환될 때 지원되지 않습니다. Django의 Range Field는 전용 range lookup을 사용해야 합니다.

--- shows/managers.py
+++ shows/managers.py
@@ -7,11 +7,11 @@ class ShowManager(models.Manager):
         return super().get_queryset().annotate(
             is_ongoing=Case(
                 # 스케줄의 시작(lower)이 현재 시각보다 클 때
-                When(schedule__lower__gt=Now(), then=Value("BEFORE")),
+                When(schedule__fully_gt=Now(), then=Value("BEFORE")),
                 # 현재 시각이 스케줄 범위 안에 포함될 때
                 When(schedule__contains=Now(), then=Value("DURING")),
                 # 스케줄의 종료(upper)가 현재 시각보다 작을 때
-                When(schedule__upper__lt=Now(), then=Value("AFTER")),
+                When(schedule__fully_lt=Now(), then=Value("AFTER")),
                 output_field=CharField()
             )
         )
