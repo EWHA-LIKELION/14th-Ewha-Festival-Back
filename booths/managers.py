960638f298d57effc399b2dc63b9e7cdc93cef5a
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2026-04-13 15:57:22 +0900

    🐛 Now()를 SQL 문자열 안으로 옮김 (#95)
    - Now()는 파라미터가 아니라 SQL 함수이므로 SQL 문자열 안에 직접 써야 합니다.

--- booths/managers.py
+++ booths/managers.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.db.models import BooleanField
 from django.db.models.expressions import RawSQL
-from django.db.models.functions import Coalesce, Now
+from django.db.models.functions import Coalesce
 
 class BoothManager(models.Manager):
     def get_queryset(self):
-        check_schedule = RawSQL("%s @> ANY(schedule)", (Now(),))
+        check_schedule = RawSQL("Now() @> ANY(schedule)")
 
         return super().get_queryset().annotate(
             is_ongoing=Coalesce(
