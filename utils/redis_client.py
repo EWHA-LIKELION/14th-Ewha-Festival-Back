4512c4118d651096f83151d9efa6645b3837dd1d
gyuuuuri <168539800+gyuuuuri@users.noreply.github.com>
2026-04-14 18:08:02 +0900

    🔥 Redis 관련 환경변수 수정 (#99)

--- utils/redis_client.py
+++ utils/redis_client.py
@@ -1,15 +1,18 @@
 import redis
 from django.conf import settings
+from urllib.parse import urlparse
 
 _client = None
 
 def get_redis_client() -> redis.Redis:
     global _client
     if _client is None:
+        url = urlparse(settings.CACHES["default"]["LOCATION"])
         _client = redis.Redis(
-            host=settings.REDIS_HOST,
-            port=settings.REDIS_PORT,
-            db=settings.REDIS_DB,
+            host=url.hostname,
+            port=url.port or 6379,
+            password=url.password,
+            db=0,
             decode_responses=True,
             socket_connect_timeout=1,
             socket_timeout=1,
