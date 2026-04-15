85c489843499c34f9fb8fd38dae38609d41b86ae
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-04-16 01:06:10 +0900

    🔨 insertshow 커맨드 추가 (#87)

---
+++ shows/management/commands/insertshow.py
@@ -0,0 +1,61 @@
+import csv
+import os
+from django.core.management.base import BaseCommand, CommandError
+from utils.helpers import tsv_file
+from utils.choices import LocationChoices
+from shows.models import Show
+from searchs.models import Location
+
+class Command(BaseCommand):
+    help = """Show 모델에 여러 개의 레코드를 삽입합니다.
+SCP로 EC2에 tsv 파일을 직접 업로드한 후에 명령어를 실행해 주세요."""
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--file',
+            type=tsv_file,
+            required=True,
+            help="tsv 파일 경로를 입력해 주세요.",
+        )
+
+    def handle(self, *args, **options):
+        file_path:str = options['file']
+
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                reader = csv.DictReader(f, delimiter='\t')
+                data_list = list(reader)
+        except Exception as e:
+            raise CommandError(f"tsv 파일을 읽는 중 오류가 발생했습니다: {e}")
+
+        show_list = list()
+        for data in data_list:
+            try:
+                location = Location.objects.get(
+                    building=LocationChoices.STUDENT_UNION,
+                    number__isnull=True,
+                )
+            except Location.DoesNotExist:
+                raise CommandError(
+                    f"Location 데이터가 올바르지 않습니다. "
+                    f"(building={LocationChoices.STUDENT_UNION}, number=Null)"
+                )
+
+            show = Show(
+                id=data['id'],
+                name=data['name'],
+                schedule=data['schedule'],
+                location=location,
+            )
+            show.set_admincode(data['admincode'])
+            show_list.append(show)
+
+        try:
+            instances = Show.objects.bulk_create(show_list)
+        except Exception as e:
+            raise CommandError(f"bulk_create 중 오류가 발생했습니다: {e}")
+
+        self.stdout.write(self.style.SUCCESS(f"Show 모델에 데이터 {len(instances)}개를 삽입했습니다."))
+
+        os.remove(file_path)
+        self.stdout.write("tsv 파일을 삭제했습니다.")
