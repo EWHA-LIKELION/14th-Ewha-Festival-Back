b99bbc8db361f4d9d494ba685a21917d81e4e9bf
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2026-04-16 21:07:03 +0900

    ✨ 관리자 인증 API 추가 (#83)
    * 🔨 관리자 인증 API path 추가 (#26)
    * 🔨 관리자 인증 API 뷰 추가 (#26)
    * 🔨 PermissionSerializer 추가 (#26)
    * 🔨 PermissionService 로직 작성 (#26)
    * 🔨 PermissionService 추가 (#26)
    * 🩹 PermissionSerializer password 필드 write_only 추가 (#26)
    * ♻️ Permission 뷰 접두사 분석 개선 (#26)
    * ♻️ PermissionService 반복 로직 정리 (#26)
    * 🐛 오타 수정 (#26)
    * ✏️ programname 변수명 변경 (#26)
    * Delete booths/migrations/0002_remove_booth_admin_code_booth_admincode.py
    * Delete shows/migrations/0002_remove_show_admin_code_show_admincode_and_more.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("logout/kakao/", KakaoLogoutView.as_view(), name="kakao_logout"),
     path('my-data/', MyDataView.as_view(), name='my-data'),
     path('my-scrap/', MyScrapView.as_view(), name='my-scrap'),
+    path('permission/', Permission.as_view(), name='permission'),
 ]
