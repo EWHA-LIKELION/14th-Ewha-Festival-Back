da62d052866a4a69c4f19705786aa455f02163e6
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2026-05-15 21:12:45 +0900

    📈 Nginx 로그 설정, Gunicorn 로그 설정, CloudWatch 데이터 다운로드 스크립트 추가 (#172)
    * 📈 Nginx 로그 설정 추가 (#164)
    
    * 📈 Gunicorn 로그 설정 추가 (#164)
    
    * 📈 CloudWatch 데이터 다운로드 스크립트 추가 (#164)

--- configs/docker/entrypoint.prod.sh
+++ configs/docker/entrypoint.prod.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p /var/log/gunicorn
+
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
---
+++ gunicorn.conf.py
@@ -0,0 +1,10 @@
+bind = "0.0.0.0:8000"
+workers = 3
+keep_alive = 75
+timeout = 120
+worker_class = "sync"
+
+accesslog = "/var/log/gunicorn/access.log"
+errorlog = "/var/log/gunicorn/error.log"
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s %(D)s'
