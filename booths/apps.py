06892b849da9099c295ce6c6524bc9787879dd99
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-05-20 00:09:28 +0900

    🔨 apps.py에 스케줄러 등록 (#91)

--- booths/apps.py
+++ booths/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class BoothsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'booths'
+
+    def ready(self):
+        from .scheduler import start
+        start()
