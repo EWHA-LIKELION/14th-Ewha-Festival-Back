4fa8159083fc2201f53db8befb1b6b11f505f557
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-05-20 01:51:57 +0900

    ♻️ 서비스 메서드를 클래스로 개선 (#91)

--- booths/scheduler.py
+++ booths/scheduler.py
@@ -5,24 +5,26 @@ from apscheduler.triggers.date import DateTrigger
 from django_apscheduler.jobstores import DjangoJobStore
 
 def start():
-    from .services import reset_ongoing
+    from .models import Booth # 순환 참조 방지
+    from .services import BoothService
 
     scheduler = BackgroundScheduler(timezone="Asia/Seoul")
     scheduler.add_jobstore(DjangoJobStore(), "default")
 
-    common = dict(
-        func=reset_ongoing,
-        replace_existing=True,  # 재시작 시 중복 등록 방지
+    booth_service = BoothService(booth_model=Booth)
+    reset_all = dict(
+        func=booth_service.reset_all,
+        replace_existing=True,
     )
 
     scheduler.add_job(
-        **common,
+        **reset_all,
         trigger=CronTrigger(hour=0, minute=0),
         id="reset_booth_ongoing_daily_0000",
         name="매일 0시 초기화",
     )
     scheduler.add_job(
-        **common,
+        **reset_all,
         trigger=DateTrigger(
             run_date=datetime(2026,5,20,18,0,0),
             timezone="Asia/Seoul"
@@ -31,7 +33,7 @@ def start():
         name="2026.05.20.(수) 18:00 초기화",
     )
     scheduler.add_job(
-        **common,
+        **reset_all,
         trigger=DateTrigger(
             run_date=datetime(2026,5,21,18,0,0),
             timezone="Asia/Seoul"
@@ -40,7 +42,7 @@ def start():
         name="2026.05.21.(목) 18:00 초기화",
     )
     scheduler.add_job(
-        **common,
+        **reset_all,
         trigger=DateTrigger(
             run_date=datetime(2026,5,22,16,0,0),
             timezone="Asia/Seoul"
--- booths/services.py
+++ booths/services.py
@@ -1,5 +1,14 @@
-def reset_ongoing():
-    """모든 부스의 ongoing을 Null로 초기화"""
-    from .models import Booth  # 순환 import 방지를 위해 내부 import
-    updated = Booth.objects.update(ongoing=None)
-    print(f"[Scheduler] {updated}개 부스 ongoing 초기화 완료")
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .models import Booth
+
+class BoothService:
+    def __init__(self, booth_model:Booth):
+        self.booth_model = booth_model
+
+    def reset_all(self):
+        """모든 부스의 ongoing을 Null로 초기화"""
+        updated_booths = self.booth_model.objects.update(ongoing=None)
+        return updated_booths
