84bb57bce280bcc21fd5ccd9f725fe4dba79e22f
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-05-20 02:02:29 +0900

    🔨 reset_early_closing_buildings 추가 (#91)

--- booths/services.py
+++ booths/services.py
@@ -1,7 +1,24 @@
+from utils.choices import LocationChoices
 from .models import Booth
+
+EARLY_CLOSING_BUILDINGS = (
+    LocationChoices.GRASS_GROUND,
+    LocationChoices.SENTENNIAL_MUSEUM,
+    LocationChoices.SPORT_TRACK,
+    LocationChoices.WELCH_RYANG_AUDITORIUM,
+)
 
 class BoothService:
     def reset_all(self):
         """모든 부스의 ongoing을 Null로 초기화"""
         updated_booths = Booth.objects.update(ongoing=None)
+        return updated_booths
+
+    def reset_early_closing_buildings(self):
+        """잔디광장, 박물관, 스포츠트랙, 대강당 부스의 ongoing을 Null로 초기화"""
+        updated_booths = (
+            Booth.objects
+            .filter(location__building__in=EARLY_CLOSING_BUILDINGS)
+            .update(ongoing=None)
+        )
         return updated_booths
