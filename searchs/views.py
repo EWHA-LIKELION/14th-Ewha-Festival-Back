b3b621cd16c803adb31e4ed74069528eda6fcf39
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2026-05-20 23:44:26 +0900

    ♻️ 스크랩 목록 조회 API 쿼리 개선 & 누락된 is_scraped 추가 (#193)
    * ⚡️ 서브쿼리에서 JOIN 방식으로 변경
    
    * 🎨 포맷팅
    
    * 🐛 #189 에서 누락한 is_scraped 추가

--- searchs/views.py
+++ searchs/views.py
@@ -32,8 +32,9 @@ def search(*, request, booths_qs, shows_qs):
     booths = (
         booths_qs
         .filter(booth_q)
-        .with_scraps_count(program="booth")
         .distinct()
+        .with_scraps_count(program="booth")
+        .with_is_scraped(request.user, program='booth')
         .filter_and_sort(request.query_params, program="booth")
     )
 
@@ -43,8 +44,9 @@ def search(*, request, booths_qs, shows_qs):
     shows = (
         shows_qs.with_name_no_space()
         .filter(show_q)
-        .with_scraps_count(program="show")
         .distinct()
+        .with_scraps_count(program="show")
+        .with_is_scraped(request.user, program='show')
         .filter_and_sort(request.query_params, program="show")
     )
 
