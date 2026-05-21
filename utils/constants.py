9a3de97a52370fe782a9c510a4437e82bb346131
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-05-21 12:39:05 +0900

    🔨 Cachekey 추가 (#194)

--- utils/constants.py
+++ utils/constants.py
@@ -5,6 +5,8 @@ class Cachekey(Enum):
     BOOTH_DETAIL = 'booth_detail:{user_id}:{booth_id}'
     SHOW_LIST = 'show_list:{user_id}:{params_hash}'
     SHOW_DETAIL = 'show_detail:{user_id}:{show_id}'
+    SEARCH_LIST = 'search_list:{user_id}:{params_hash}'
+    SCRAP_LIST = 'scrap_list:{user_id}:{params_hash}'
 
     def format(self, **kwargs):
         return self.value.format(**kwargs)
