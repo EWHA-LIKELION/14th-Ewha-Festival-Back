4275df0ba93cc19938ba5aa372beae0c6757d78d
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-05-22 02:02:26 +0900

    🎨 포맷팅 (#200)

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,6 @@
+from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
